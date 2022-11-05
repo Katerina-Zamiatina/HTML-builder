5638e8659679dde57fab9bf2e4d816ca5525bb8b
Katerina Zamiatina <eozamyatina@gmail.com>
2022-11-05 14:49:41 +0100

    refactor: solution 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,28 +1,59 @@
-const fs = require("fs");
+const fs = require("fs").promises;
+// const fsProm = fs.promises;
 const path = require("path");
 const chalk = require("chalk");
 
 const folderPath = path.join(__dirname, "secret-folder");
 
-async function showFileList() {
-  try {
-    await fs.readdir(folderPath, { withFileTypes: true }, (err, data) => {
-      data.forEach((file) => {
-        const filePath = path.resolve(__dirname, "secret-folder", file.name);
-        fs.stat(filePath, (err, stats) => {
-          let fileInfo = [];
-          const fileName = file.name.split(".").slice(0, 1).join(".");
-          const extension = path.extname(file.name).slice(1);
-          const size = stats.size;
-          fileInfo.push(fileName, extension, size);
+// showFilesList refactor
 
-          console.log(chalk.green(fileInfo.join(" - ")));
-        });
-      });
+async function showFilesList() {
+  try {
+    const data = await fs.readdir(folderPath, { withFileTypes: true });
+
+    data.forEach(async (file) => {
+      if (!file.isDirectory()) {
+        let fileData = [];
+        const filePath = path.resolve(__dirname, "secret-folder", file.name);
+        const fileName = file.name.split(".").slice(0, 1).join(".");
+        const extension = path.extname(file.name).slice(1);
+
+        const stats = await fs.stat(filePath);
+        const size = (Number(stats.size) / 2000).toFixed(3) + "kb";
+        fileData.push(fileName, extension, size);
+        console.log(chalk.green(fileData.join(" - ")));
+      }
     });
-  } catch (err) {
+  } catch (error) {
     console.error(chalk.red("ERROR: \n"), err.message);
   }
 }
 
-showFileList();
+showFilesList();
+
+// async function showFileList() {
+//   try {
+//     await fs.readdir(folderPath, { withFileTypes: true }, (err, data) => {
+//       data.forEach((file) => {
+
+//         if (file.isFile()) {
+//           const filePath = path.resolve(__dirname, "secret-folder", file.name);
+//           fs.stat(filePath, (err, stats) => {
+//             let fileInfo = [];
+//             const fileName = file.name.split(".").slice(0, 1).join(".");
+//             const extension = path.extname(file.name).slice(1);
+//             const size = stats.size;
+
+//             fileInfo.push(fileName, extension, size);
+
+//             console.log(chalk.green(fileInfo.join(" - ")));
+//           });
+//         }
+//       });
+//     });
+//   } catch (err) {
+//     console.error(chalk.red("ERROR: \n"), err.message);
+//   }
+// }
+
+// showFileList();
