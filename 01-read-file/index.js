5638eefbabca5a52747933d1217eca0b95ad2f1c
Katerina Zamiatina <eozamyatina@gmail.com>
2022-11-06 19:43:23 +0100

    fix: rewrite to ReadStream 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,13 @@
-const fs = require("fs").promises;
+const fs = require("fs");
 const path = require("path");
 const { stdout } = process;
 
 const textPath = path.join(__dirname, "text.txt");
+const fileStream = fs.createReadStream(textPath, "utf8");
 
-fs.readFile(textPath)
-  .then((data) => stdout.write(data.toString()))
-  .catch((err) => console.log("ERROR", err));
+fileStream.on('data', data=> stdout.write(data))
+
+// fs.promises
+//   .readFile(textPath)
+//   .then((data) => stdout.write(data.toString()))
+//   .catch((err) => console.log("ERROR", err));
